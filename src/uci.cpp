5ed88d9ef17eb48b3f8315bae9ff301df8274ef8
aary <aaryadevv8@gmail.com>
2026-02-22 18:19:46 +1100

    Modify help message for Solace chess engine
    Updated help text to reflect changes from Stockfish to Solace.

--- src/uci.cpp
+++ src/uci.cpp
@@ -164,12 +164,11 @@ void UCIEngine::loop() {
         }
         else if (token == "--help" || token == "help" || token == "--license" || token == "license")
             sync_cout
-              << "\nStockfish is a powerful chess engine for playing and analyzing."
+              << "\nSolace is a hyper-aggressive UCI chess engine forked from Stockfish."
                  "\nIt is released as free software licensed under the GNU GPLv3 License."
-                 "\nStockfish is normally used with a graphical user interface (GUI) and implements"
+                 "\nSolace is normally used with a graphical user interface (GUI) and implements"
                  "\nthe Universal Chess Interface (UCI) protocol to communicate with a GUI, an API, etc."
-                 "\nFor any further information, visit https://github.com/official-stockfish/Stockfish#readme"
-                 "\nor read the corresponding README.md and Copying.txt files distributed along with this program.\n"
+                 "\nFor any further information, see the README.md and Copying.txt files distributed along with this program.\n"
               << sync_endl;
         else if (!token.empty() && token[0] != '#')
             sync_cout << "Unknown command: '" << cmd << "'. Type help for more information."
